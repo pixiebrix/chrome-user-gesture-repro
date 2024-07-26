77bee15eacdf24b8a1dc146b610e41447a585755
Stephen Bussey <sb8244@cs.ship.edu>
2024-07-25 20:03:25 -0400

    Reproduce the root issue
    * If mousedown is used to send a message to the service worker
    * AND the service worker listens to that message and calls chrome.tabs.sendMessage
    * AND the content script is listening for messages
    * THEN the issue happens and "no user gesture" error will appear

--- dist/serviceWorker.js
+++ dist/serviceWorker.js
@@ -1,23 +1,28 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  void chrome.sidePanel.setOptions({
-    tabId: sender.tab.id,
-    path: "sidepanel.html",
-    enabled: true,
-  });
+  if (request.command === "open") {
+    void chrome.sidePanel.setOptions({
+      tabId: sender.tab.id,
+      path: "sidepanel.html",
+      enabled: true,
+    });
 
-  chrome.sidePanel.open({ tabId: sender.tab.id }).then(
-    () => {
-      sendResponse({ success: "success" });
-    },
-    (error) => {
-      sendResponse({
-        error: error.message,
-      });
-      throw error;
-    },
-  );
+    chrome.sidePanel.open({ tabId: sender.tab.id }).then(
+      () => {
+        sendResponse({ success: "success" });
+      },
+      (error) => {
+        sendResponse({
+          error: error.message,
+        });
+        throw error;
+      }
+    );
 
-  return true;
+    return true;
+  } else if (request.command === "repro") {
+    // The contents do not matter
+    chrome.tabs.sendMessage(sender.tab.id, "JUNK");
+  }
 });
 
 console.log("Background script loaded.");
